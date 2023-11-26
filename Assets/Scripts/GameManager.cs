ef4cdcf577be9334746e5d6f4adb1c185ab704a4
Kyle Cuss <cussprogramming@gmail.com>
2023-11-26 16:46:24 -0330

    Started GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public int health = 100;
+    public int money = 500;
+
+    [Header("Components")] 
+    public TextMeshProUGUI healthAndMoneyText;
+
+    [Header("Events")] 
+    public UnityEvent onEnemyDestroyed;
+    public UnityEvent onMoneyChanged;
+
+    //Singleton
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddMoney(int amount)
+    {
+        money += amount;
+        UpdateHealthAndMoneyText();
+        
+        onMoneyChanged.Invoke();
+    }
+    
+    public void TakeMoney(int amount)
+    {
+        money -= amount;
+        UpdateHealthAndMoneyText();
+        
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        UpdateHealthAndMoneyText();
+
+        if (health <= 0)
+            GameOver();
+    }
+    
+    public void OnEnemyDestroyed()
+    {
+        
+    }
+    
+    void UpdateHealthAndMoneyText()
+    {
+        healthAndMoneyText.text = $"Health: {health} \nMoney: {money}";
+    }
+
+    void GameOver()
+    {
+        
+    }
+
+    void WinGame()
+    {
+        
+    }
+}
